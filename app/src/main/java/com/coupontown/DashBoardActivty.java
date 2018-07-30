b7891f6f0b266c40a82269b043fa64007493f231
Pavan.Kulkarni <z002qgl@8c85908a91fb.target.com>
2018-07-30 23:07:01 +0530

    Added the App redirect

--- app/src/main/java/com/coupontown/DashBoardActivty.java
+++ app/src/main/java/com/coupontown/DashBoardActivty.java
@@ -1,23 +1,23 @@
 package com.coupontown;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ExpandableListAdapter;
-import android.widget.ExpandableListView;
-import android.widget.Toast;
+import android.widget.*;
+import com.coupontown.model.OfferList;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -76,6 +76,10 @@ public class DashBoardActivty extends AppCompatActivity
                                 expandableListTitle.get(i)).get(
                                 i1), Toast.LENGTH_SHORT
                 ).show();
+                final String appPackageName = getPackageName();
+                Log.i("Package Name" , appPackageName);
+
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://paytm.com")));
                 return false;
             }
 
@@ -175,4 +179,7 @@ public class DashBoardActivty extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+  
 }
